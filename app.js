27fbe686d6d5620f7093f81597fcece77ffff3d7
Tejas Borkar <76744372+tejas-borkar22@users.noreply.github.com>
2021-01-15 22:30:08 +0530

    input / output from user

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-console.log("script is running");
-var btnTranslate = document.querySelector("#btn-translator");
+var btnTranslate = document.querySelector("#btn-translator"); // Selecting button with ID
+var txtInput = document.querySelector("#txt-Ip");    // Selecting text-area with ID
+/*var btnSuccess = document.querySelector(".btn-success");  // Selecting button with class
+var translation = document.querySelector("[name=translated]"); //Selecting text-area with name
+console.log(btnSuccess);
+console.log(translation); 
+console.log(txtInput);  */
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked");
-})
+function clickHandler() {                    //Call back function
+    outputDiv.innerText = "Translated ="+txtInput.value;
+};
+
+btnTranslate.addEventListener("click", clickHandler);    //Calling the function after click event
