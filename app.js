fec9795009d352625687a76517b4116fd3db41da
Tejas Borkar <76744372+tejas-borkar22@users.noreply.github.com>
2021-01-15 17:27:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("script is running");
+var btnTranslate = document.querySelector("#btn-translator");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked");
+})
