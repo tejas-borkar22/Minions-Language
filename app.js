06150da4930d0dee95d7de8f96e955f80ce99210
Tejas Borkar <76744372+tejas-borkar22@users.noreply.github.com>
2021-01-19 17:24:51 +0530

    feat : url added

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 var btnTranslate = document.querySelector("#btn-translator"); // Selecting button with ID
 var txtInput = document.querySelector("#txt-Ip");    // Selecting text-area with ID
-/*var btnSuccess = document.querySelector(".btn-success");  // Selecting button with class
+
+/*
+var btnSuccess = document.querySelector(".btn-success");  // Selecting button with class
 var translation = document.querySelector("[name=translated]"); //Selecting text-area with name
-console.log(btnSuccess);
-console.log(translation); 
-console.log(txtInput);  */
+*/
+
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {                    //Call back function
-    outputDiv.innerText = "Translated ="+txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
 };
 
-btnTranslate.addEventListener("click", clickHandler);    //Calling the function after click event
+function catchErrors(error){
+    console.log("Error Occurred", error);
+    alert("Please try agian after some time")
+};
+
+function clickHandler() {                   
+
+    var inputTxt = txtInput.value;  //Taking input from user
+
+    // Calling Server for processing
+    fetch(getTranslationURL(inputTxt))
+    .then(res => res.json())
+    .then(json =>{var translated = (json.contents.translated);
+         outputDiv.innerText = translated;
+    })
+    .catch(catchErrors);                     //catchErrors is a callback function
+};
+
+btnTranslate.addEventListener("click", clickHandler);    //Calling the function after click event, clickHandler is a callback function
+
